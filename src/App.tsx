2e240a718eeda72e2e6ea54a8cb4db54cf7ab6dc
ISRAELFCOTA <israelf.cota1203@gmail.com>
2024-02-25 23:04:56 -0300

    refact: home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { ThemeProvider } from "styled-components";
 import "./App.css";
 import GlobalStyle from "./globalStyle";
 import { theme } from "./theme/theme";
+import { Home } from "./screens/Home";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Home />
     </ThemeProvider>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,2 +1,36 @@
 import styled from "styled-components";
-export const Container = styled.div``;
+export const Container = styled.div`
+  flex: 1;
+`;
+
+export const HeaderWrapper = styled.div`
+  padding: 0.25rem;
+  margin: 1.5rem;
+`;
+export const HeaderTitle = styled.h3`
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
+export const ShowTasksWrapper = styled.div`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.modalBackgroundColor};
+  border-top: 0.1rem solid ${({ theme }) => theme.colors.modalBorderColor};
+  border-left: 0.1rem solid ${({ theme }) => theme.colors.modalBorderColor};
+  border-right: 0.1rem solid ${({ theme }) => theme.colors.modalBorderColor};
+  border-radius: 5px 5px 0 0;
+  margin: 1.5rem;
+`;
+
+export const ShowTasksHeader = styled.div`
+  margin-left: 1rem;
+  flex-direction: row;
+  align-items: center;
+  display: flex;
+  gap: 0.75rem;
+`;
+
+export const ShowTasksHeaderTitle = styled.h3`
+  font-size: 1rem;
+  font-weight: bold;
+`;
