134b699e7e2fc50afce1a715321b00a9a898ab8a
Israel Fernandes <israelfernandescota702@hotmail.com>
2024-02-26 12:30:57 -0300

    refactor: Icon header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { ShowTask } from "../../components/ShowTask";
+import { IconList } from "@tabler/icons-react";
+import { useTheme } from "styled-components";
 import * as S from "./styles";
 
 export const Home: React.FC = () => {
+  const theme = useTheme();
   return (
     <S.Container>
       <S.HeaderWrapper>
@@ -10,24 +13,7 @@ export const Home: React.FC = () => {
       </S.HeaderWrapper>
       <S.ShowTasksWrapper>
         <S.ShowTasksHeader>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="icon icon-tabler icon-tabler-notes"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="#9f1faa"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M5 3m0 2a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z" />
-            <path d="M9 7l6 0" />
-            <path d="M9 11l6 0" />
-            <path d="M9 15l4 0" />
-          </svg>
+          <IconList color={theme.colors.lightMain} />
           <S.ShowTasksHeaderTitle>Tarefas</S.ShowTasksHeaderTitle>
         </S.ShowTasksHeader>
         <ShowTask />
