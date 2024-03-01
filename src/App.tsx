77ca8ebee87271cd18acdaff83c4c5b3dc08f3ba
ISRAELFCOTA <israelf.cota1203@gmail.com>
2024-03-01 16:26:04 -0300

    feat: add toast

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,25 @@ import "./App.css";
 import GlobalStyle from "./globalStyle";
 import { theme } from "./theme/theme";
 import { Home } from "./screens/Home";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <ToastContainer
+        position="bottom-left"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
       <Home />
     </ThemeProvider>
   );
