a9453042102c4e3763cb65787c8bdf85cef66eee
ISRAELFCOTA <israelf.cota1203@gmail.com>
2024-03-01 20:20:51 -0300

    feat: new libs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@assets": "/src/assets/",
+      "@components": "/src/components/",
+      "@service": "/src/service/",
+      "@screens": "/src/screens/",
+      "@dto": "/src/dto/",
+      "@theme": "/src/theme/",
+    },
+  },
+});
