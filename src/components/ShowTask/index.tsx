a20af688ff11d8baab6557f99bbf268b4d0ee1a2
ISRAELFCOTA <israelf.cota1203@gmail.com>
2024-03-01 16:27:44 -0300

    refactor: add toast and trade icons

--- src/components/ShowTask/index.tsx
+++ src/components/ShowTask/index.tsx
@@ -1,47 +1,66 @@
 import { memo, useCallback, useState } from "react";
-import { IconSettings, IconTrash } from "@tabler/icons-react";
+import { IconEdit, IconTrash, IconZoomInArea } from "@tabler/icons-react";
 import { TaskDto } from "../../dto/task";
-import * as S from "./style";
 import { HttpService } from "../../service/httpService";
-import { ModalEdit } from "../ModalEdit";
+import { toast } from "react-toastify";
+import * as S from "./style";
+import { ClipLoader } from "react-spinners";
 
 type Props = {
   item: TaskDto;
+  fetchData: () => Promise<void>;
+  onClose: () => void;
+  onShowModalEdit: () => void;
+  onShowModalInfo: () => void;
 };
 
-export const ShowTask = memo(({ item }: Props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [showModal, setShowModal] = useState(false);
+export const ShowTask = memo(
+  ({ item, fetchData, onClose, onShowModalEdit, onShowModalInfo }: Props) => {
+    const [isLoading, setIsLoading] = useState(false);
 
-  const handleDelete = useCallback(async () => {
-    setIsLoading(true);
-    try {
-      await HttpService.delete(`tasks/${item.task_id}`);
-      console.log("deletou");
-    } catch (error) {
-      console.log("Erro ao excluir a tarefa:", error);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [item.task_id]);
+    const handleDelete = useCallback(async () => {
+      setIsLoading(true);
+      try {
+        await HttpService.delete(`tasks/${item.task_id}`);
+        await fetchData();
+        onClose();
+        toast.success("Tarefa excluída com sucesso!");
+      } catch (error) {
+        toast.error("Não foi possivel excluir a tarefa.");
+      } finally {
+        setIsLoading(false);
+      }
+    }, [fetchData, item.task_id, onClose]);
 
-  return (
-    <>
-      <S.Container>
-        <S.TaskWrapper>
-          <S.TaskTitle>{item.taskName}</S.TaskTitle>
-          <S.ButtonWrapper>
-            <S.Button onClick={() => setShowModal(true)}>
-              <IconSettings />
-            </S.Button>
-            <S.Button onClick={handleDelete}>
-              <IconTrash />
-            </S.Button>
-          </S.ButtonWrapper>
-        </S.TaskWrapper>
-        <S.TaskSeparator />
-      </S.Container>
-      {showModal && <ModalEdit item={item} />}
-    </>
-  );
-});
+    return (
+      <>
+        <S.Container>
+          <S.TaskWrapper>
+            <S.TaskTitle>{item.taskName}</S.TaskTitle>
+            <S.ButtonWrapper>
+              <S.Button onClick={onShowModalInfo}>
+                <IconZoomInArea />
+              </S.Button>
+              <S.Button onClick={onShowModalEdit}>
+                <IconEdit />
+              </S.Button>
+
+              <S.Button onClick={handleDelete} disabled={isLoading}>
+                {isLoading ? (
+                  <ClipLoader
+                    color="#fff"
+                    size={24}
+                    cssOverride={{ marginInline: "0.75rem" }}
+                  />
+                ) : (
+                  <IconTrash />
+                )}
+              </S.Button>
+            </S.ButtonWrapper>
+          </S.TaskWrapper>
+          <S.TaskSeparator />
+        </S.Container>
+      </>
+    );
+  }
+);
