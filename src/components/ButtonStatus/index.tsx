44403bc46d71420da3382875e432dbe091a84747
ISRAELFCOTA <israelf.cota1203@gmail.com>
2024-03-01 16:26:32 -0300

    refactor: button statatus

--- src/components/ButtonStatus/index.tsx
+++ src/components/ButtonStatus/index.tsx
@@ -1,22 +1,29 @@
-import { TaskStatus } from "../../dto/task";
-import { Button } from "./styles";
+import { TaskStatus, TaskStatusType } from "../../dto/task";
+import * as S from "./styles";
 
 type Props = {
-  taskStatus: TaskStatus;
+  taskStatus: TaskStatusType;
+  onClick: () => void;
+  isSelected: boolean;
 };
 
-export const ButtonStatus = ({ taskStatus }: Props) => {
-  const colors: Record<TaskStatus, string> = {
-    active: "green",
-    idle: "yellow",
-    success: "red",
+export const ButtonStatus = ({ taskStatus, onClick, isSelected }: Props) => {
+  const colors: Record<TaskStatus, { bgColor: string; color: string }> = {
+    2: { bgColor: "#FFEA00", color: "#120016" },
+    3: { bgColor: "#BD0000", color: "#D0D0D0" },
+    1: { bgColor: "#18B600", color: "#021600" },
   };
 
-  const color = colors[taskStatus];
+  const taskColor = colors[taskStatus.status];
 
   return (
-    <>
-      <Button color={color} />
-    </>
+    <S.Button
+      bgColor={taskColor.bgColor}
+      onClick={onClick}
+      disabled={isSelected}
+      isSelect={isSelected}
+    >
+      <S.Label color={taskColor.color}>{taskStatus.label}</S.Label>
+    </S.Button>
   );
 };
--- src/components/ButtonStatus/styles.ts
+++ src/components/ButtonStatus/styles.ts
@@ -1,13 +1,37 @@
-import styled from "styled-components";
-
-export const Button = styled.button<{ color: string }>`
-  background-color: ${({ color }) => color};
+import styled, { css } from "styled-components";
+type Props = {
+  bgColor: string;
+  isSelect: boolean;
+};
+export const Button = styled.button<Props>`
+  background-color: ${({ bgColor }) => bgColor};
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 1rem;
-  height: 1rem;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
   border: none;
   cursor: pointer;
-  border-radius: 50%;
+  border-radius: 0.5rem;
+  gap: 1rem;
+  transition: opacity 100ms linear;
+  &:active {
+    opacity: 0.5;
+  }
+  &:hover {
+    opacity: 0.75;
+  }
+  ${({ isSelect }) =>
+    isSelect &&
+    css`
+      transition: transform 50ms linear;
+      transform: scale(1.3);
+    `}
+`;
+
+export const Label = styled.p<{ color: string }>`
+  font-weight: 600;
+  color: ${({ color }) => color};
 `;
